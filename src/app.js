68199e98e421c36b17d45df293975d3ba2f101f1
Akshay Pathak <akshaypathak0821@gmail.com>
2026-07-15 15:50:06 +0530

    controller added

--- src/app.js
+++ src/app.js
@@ -12,7 +12,17 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
+
+
+//importing router
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+// https://localhost:8000/api/v1/users/register
+// https://localhost:8000/api/v1/users/login
 
 
 export { app };
