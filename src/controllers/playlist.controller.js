b4af2055985a13106046fcf40a121efae38fcd55
Akshay Pathak <akshaypathak0821@gmail.com>
2026-08-11 22:23:13 +0530

    Add getPlaylistById functionality with pagination

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -34,3 +34,106 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .status(201)
     .json(new APIResponse(201, playlist, "Playlist created successfully!"));
 });
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!playlistId) {
+    throw new APIError(400, "Playlist id is required!");
+  }
+
+  if (!isValidObjectId(playlistId)) {
+    throw new APIError(400, "Invalid playlist id!");
+  }
+
+  const { page = 1, limit = 10, sortType = "desc" } = req.query;
+
+  //   const playlistExists = await Playlist.findById(playlist);
+
+  //   if(!playlistExists){
+  //     throw new APIError(404, "Playlist not found!");
+  //   }
+
+  const pageNumber = Number(page);
+  const limitNumber = Number(limit);
+  const skip = (pageNumber - 1) * limitNumber;
+
+  const playlist = await Playlist.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(playlistId),
+      },
+    },
+
+    {
+      $addFields: {
+        totalVideos: {
+          $size: "$videos",
+        },
+      },
+    },
+
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videosDetails",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "userDetails",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          {
+            $unwind: "$userDetails",
+          },
+
+          {
+            $sort: { createdAt: sortType === "asc" ? 1 : -1 },
+          },
+
+          {
+            $skip: skip,
+          },
+
+          {
+            $limit: limitNumber,
+          },
+
+          {
+            $project: {
+              title: 1,
+              thumbnail: 1,
+              createdAt: 1,
+              userDetails: 1,
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  if (playlist.length === 0) {
+    throw new APIError(404, "Playlist not found!");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, "User playlist fetched successfully!")
+    );
+});
