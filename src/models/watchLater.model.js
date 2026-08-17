dd33efc021f0f415b0bfb0136288c28c354d0473
Akshay Pathak <akshaypathak0821@gmail.com>
2026-08-17 17:19:55 +0530

    feat: create WatchLater model with schema and pagination support

---
+++ src/models/watchLater.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const watchLaterSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+
+    savedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+watchLaterSchema.index({ video: 1, savedBy: 1 }, { unique: true });
+
+watchLaterSchema.plugin(mongooseAggregatePaginate);
+
+export const WatchLater = mongoose.model("WatchLater", watchLaterSchema);
