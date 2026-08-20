1575d5833c3b6de0d722cb0379506949cef714b7
Akshay Pathak <akshaypathak0821@gmail.com>
2026-08-20 19:37:42 +0530

    feat: implement getAllSentInvitations function for fetching sent invitations

--- src/controllers/playlistCollaborator.controller.js
+++ src/controllers/playlistCollaborator.controller.js
@@ -490,6 +490,86 @@ const getAllAcceptedInvitations = asyncHandler(async (req, res) => {
     );
 });
 
+const getAllSentInvitations = asyncHandler(async (req, res) => {
+  if (!req.user) {
+    throw new APIError(401, "Unauthorized request!");
+  }
+
+  const { page = 1, limit = 10, sortType = "desc" } = req.query;
+
+  const invitations = await PlaylistCollaborator.aggregatePaginate(
+    PlaylistCollaborator.aggregate([
+      {
+        $match: {
+          invitedBy: new mongoose.Types.ObjectId(req.user._id),
+        },
+      },
+
+      {
+        $sort: {
+          createdAt: sortType === "asc" ? 1 : -1,
+        },
+      },
+
+      {
+        $lookup: {
+          from: "playlists",
+          localField: "playlist",
+          foreignField: "_id",
+          as: "playlistDetails",
+          pipeline: [
+            {
+              $project: {
+                name: 1,
+              },
+            },
+          ],
+        },
+      },
+
+      {
+        $unwind: "$playlistDetails",
+      },
+
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "userDetails",
+          pipeline: [
+            {
+              $project: {
+                username: 1,
+                fullName: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+
+      {
+        $unwind: "$userDetails",
+      },
+    ]),
+    {
+      page: Number(page),
+      limit: Number(limit),
+    }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new APIResponse(
+        200,
+        invitations,
+        "Sent invitations fetched successfully!"
+      )
+    );
+});
+
 export {
   sendInvitation,
   acceptInvitation,
@@ -500,4 +580,5 @@ export {
   getAllInvitations,
   getAllPendingInvitations,
   getAllAcceptedInvitations,
+  getAllSentInvitations,
 };
